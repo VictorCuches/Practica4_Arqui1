055083c64124544f3a980bb2ae5af27ca90c79d8
Victor Cuches de León <69135983+VictorCuches@users.noreply.github.com>
2021-10-07 02:05:03 -0600

    Comando abrir y contar
    - Abrir archivo
    - Comando contar
    - Reconocer palabra despues de _
    
    * prop_ se puede hacer con lo que tengo

--- Practica4_201807307/Codigo_asm/macros.asm
+++ Practica4_201807307/Codigo_asm/macros.asm
@@ -55,7 +55,7 @@ veriABRIR macro
         ; sino son iguales
         jnz diferente
 
-        cmp bl, "$" ;si es igual a dolar
+        cmp bl, "_" ;si es igual a _
         jz iguales
         ; si son iguales
         inc di
@@ -64,6 +64,8 @@ veriABRIR macro
     iguales:
         ;en esta parte hace las acciones respectivas del comando 
         mov ncomando, 1d
+        inc di
+        mov indiceI, di
         
         jmp final
     diferente: 
@@ -84,7 +86,7 @@ veriCONTAR macro
         ; sino son iguales
         jnz diferente
 
-        cmp bl, "$" ;si es igual a dolar
+        cmp bl, "_" ;si es igual a dolar
         jz iguales
         ; si son iguales
         inc di
@@ -93,6 +95,8 @@ veriCONTAR macro
     iguales:
         ;en esta parte hace las acciones respectivas del comando 
         mov ncomando, 2d
+        inc di
+        mov indiceJ, di
         
         jmp final
     diferente: 
@@ -200,7 +204,7 @@ veriDIPTO macro
         ; sino son iguales
         jnz diferente
 
-        cmp bl, "$" ;si es igual a dolar
+        cmp bl, "_" ;si es igual a dolar
         jz iguales
         ; si son iguales
         inc di
@@ -209,6 +213,8 @@ veriDIPTO macro
     iguales:
         ;en esta parte hace las acciones respectivas del comando 
         mov ncomando, 6d
+        inc di
+        mov indiceJ, di
         
         jmp final
     diferente: 
@@ -229,7 +235,7 @@ veriHIATO macro
         ; sino son iguales
         jnz diferente
 
-        cmp bl, "$" ;si es igual a dolar
+        cmp bl, "_" ;si es igual a dolar
         jz iguales
         ; si son iguales
         inc di
@@ -238,6 +244,8 @@ veriHIATO macro
     iguales:
         ;en esta parte hace las acciones respectivas del comando 
         mov ncomando, 7d
+        inc di
+        mov indiceJ, di
         
         jmp final
     diferente: 
@@ -258,7 +266,7 @@ veriTRIPTO macro
         ; sino son iguales
         jnz diferente
 
-        cmp bl, "$" ;si es igual a dolar
+        cmp bl, "_" ;si es igual a dolar
         jz iguales
         ; si son iguales
         inc di
@@ -267,6 +275,8 @@ veriTRIPTO macro
     iguales:
         ;en esta parte hace las acciones respectivas del comando 
         mov ncomando, 8d
+        inc di
+        mov indiceJ, di
         
         jmp final
     diferente: 
@@ -275,4 +285,651 @@ veriTRIPTO macro
     final:  
 endm
 
+abrirArchivo macro rutaArch
+	mov ah, 3Dh
+	mov al, 2
+	mov dx, offset ruta
+	int 21h
+	mov handle, ax
+endm
 
+
+cerrarArchivo macro idArchivo
+	mov ah, 3Eh
+	mov bx, idArchivo
+	int 21h
+endm
+
+getRuta macro 
+    local cicloo, fini
+    
+    ;limpiando registros para que todo funcione correctamente
+    xor bl, bl
+    xor si, si 
+    ;di pasa a tener el indice donde ya termine el comando_
+    mov di, indiceI
+
+    cicloo: 
+        mov bl, readTeclado[di] ;entrada desde donde termina el comando_
+        mov ruta[si], bl ;colocando en la variable ruta la entrada 
+        cmp bl, "$"    
+        jz fini
+        inc di
+        inc si
+        jmp cicloo
+    fini:    
+endm
+
+getWord macro 
+    local cicloo, fini
+    
+    ;limpiando registros para que todo funcione correctamente
+    xor bl, bl
+    xor si, si 
+    ;di pasa a tener el indice donde ya termine el comando_
+    mov di, indiceJ
+
+    cicloo: 
+        mov bl, readTeclado[di] ;entrada desde donde termina el comando_
+        mov nextWord[si], bl ;colocando en la variable nextWord la entrada 
+        cmp bl, "$"    
+        jz fini
+        inc di
+        inc si
+        jmp cicloo
+    fini:    
+endm
+
+identifyDipto macro palabra ;paisaje
+    local ciclo, ciclo2, ciclo3, ciclo4, ciclo5, fini
+    local decreciente, creciente, homogeneo, finPalabra, otroDip
+    xor si, si
+    xor di, di
+
+    ciclo:
+        mov al, palabra[si]
+        cmp al, 97 ; a
+        jz ciclo2
+
+        cmp al, 101 ; e
+        jz ciclo2
+
+        cmp al, 111 ; o
+        jz ciclo2
+        
+        cmp al, 105 ; i
+        jz ciclo3
+
+        cmp al, 117 ; u
+        jz ciclo4
+
+        cmp palabra[si], "$"
+        jz ciclo5
+
+        inc si
+        jmp ciclo
+
+    ciclo2:
+        inc si
+        mov al, palabra[si]
+        cmp al, 105 ;i
+        jz decreciente
+
+        cmp al, 117;u
+        jz decreciente
+
+        
+        jmp ciclo
+
+    ciclo3: 
+        inc si
+        ;diptongo creciente
+        mov al, palabra[si]
+        cmp al, 97 ; a
+        jz creciente
+
+        cmp al, 101 ; e
+        jz creciente
+
+        cmp al, 111 ; o
+        jz creciente
+
+        ;diptongo homogeneo
+        cmp al, 117 ;u
+        jz homogeneo
+
+        jmp ciclo ;ciclo
+    
+    ciclo4:
+        inc si
+        ;diptongo creciente
+        mov al, palabra[si]
+        cmp al, 97 ; a
+        jz creciente
+
+        cmp al, 101 ; e
+        jz creciente
+
+        cmp al, 111 ; o
+        jz creciente
+
+        ;diptongo homogeneo
+        cmp al, 105 ;i
+        jz homogeneo
+
+        jmp ciclo ;ciclo
+
+    decreciente:
+
+        cmp flagCount, 1d
+        jz otroDip
+
+        
+
+        imprimir salto
+        imprimir wordDip
+        imprimir decrec
+        imprimir salto
+        jmp fini
+    
+    creciente:
+        cmp flagCount, 1d
+        jz otroDip
+
+    
+        imprimir salto
+        imprimir wordDip
+        imprimir crecien
+        imprimir salto
+        jmp fini
+    
+    homogeneo:
+        cmp flagCount, 1d
+        jz otroDip
+
+        
+
+        imprimir salto
+        imprimir wordDip
+        imprimir homoge
+        imprimir salto
+        jmp fini
+
+    otroDip:
+       inc numDip
+       jmp fini
+    ciclo5:
+        cmp flagCount, 1d
+        jz fini
+
+        imprimir salto
+        imprimir noWord
+        imprimir wordDip
+        imprimir salto
+   
+    fini:
+
+    
+
+
+endm
+
+
+identifyHiato macro palabra
+    local ciclo, ciclo2, ciclo3, ciclo4, ciclo5, finito
+    local simple, otroHia
+
+    xor si, si
+    xor di, di
+
+    ciclo:
+        mov al, palabra[si]
+        mov oldVoc, al
+        cmp al, 97 ; a
+        jz ciclo2
+
+        cmp al, 101 ; e
+        jz ciclo2
+
+        cmp al, 111 ; o
+        jz ciclo2
+        
+        cmp al, 105 ; i
+        jz ciclo3
+
+        cmp al, 117 ; u
+        jz ciclo5
+
+        cmp palabra[si], "$"
+        jz ciclo4
+
+        inc si
+        jmp ciclo
+
+    ciclo2:
+        inc si
+        mov al, palabra[si]
+       
+        cmp al, 97 ; a
+        jz simple
+
+        cmp al, 101 ; e
+        jz simple
+
+        cmp al, 111 ; o
+        jz simple
+
+        jmp ciclo
+
+    ciclo3:
+        inc si
+        mov al, palabra[si]
+        cmp al, 105 ; i
+        jz simple
+        jmp ciclo
+
+    ciclo5:
+        inc si
+        mov al, palabra[si]
+
+
+        cmp al, 117 ; u
+        jz simple
+
+        jmp ciclo
+
+
+
+        
+    ciclo4:
+        cmp flagCount, 1d
+        jz finito
+        imprimir salto 
+        imprimir noHiato
+        imprimir salto
+        jmp finito
+    simple:
+        cmp flagCount, 1d
+        jz otroHia
+
+        imprimir salto
+        imprimir hiatoS
+        imprimir salto
+        jmp finito
+
+    otroHia:
+        inc numHia
+        jmp finito
+    finito:
+
+
+
+endm
+
+identifyTripto macro palabra
+    local ciclo, ciclo2, ciclo3, ciclo4, finito, triptongo, otroTrip
+    xor si, si
+    xor di, di
+    
+    ciclo: 
+        mov al, palabra[si]
+        
+        cmp al, 105 ; i
+        jz ciclo2
+
+        cmp al, 117 ; u
+        jz ciclo2
+
+        cmp palabra[si], "$"
+        jz ciclo4
+
+        inc si
+        jmp ciclo
+    
+    ciclo2:
+        inc si 
+        mov al, palabra[si]
+
+        cmp al, 97 ; a
+        jz ciclo3
+
+        cmp al, 101 ; e
+        jz ciclo3
+
+        cmp al, 111 ; o
+        jz ciclo3
+
+        jmp ciclo
+    
+    ciclo3:
+        inc si
+        mov al, palabra[si]
+        
+        cmp al, 105 ; i
+        jz triptongo
+
+        cmp al, 117 ; u
+        jz triptongo
+        
+        jmp ciclo
+
+    triptongo:
+        cmp flagCount, 1d
+        jz otroTrip
+
+        
+        imprimir salto
+        imprimir tripton
+        imprimir salto
+        jmp finito
+
+    ciclo4:
+        cmp flagCount, 1d
+        jz finito
+        imprimir salto
+        imprimir noWord
+        imprimir tripton
+        imprimir salto
+        jmp finito
+    
+    otroTrip:
+        mov flagYa, 1d ;para que no revise si es diptongo la palabra
+        inc numTrip
+        jmp finito
+    finito:
+
+
+endm
+
+;IDENTIFICA QUE UNA LETRA SEA VOCAL PUEDE SER UTIL
+; identifyDipto macro palabra ;paisaje
+;     local ciclo, ciclo2, ciclo3, ciclo4, ciclo5, fini
+;     local decreciente, creciente, homogenea, finPalabra
+;     xor si, si
+;     xor di, di
+
+;     ciclo: 
+;         mov bl, vabierta[si] ;
+;         cmp palabra[di], bl
+;         ;mov al, palabra[di]
+;         jz ciclo2
+
+;         cmp si, 2
+;         jz ciclo3
+
+;         inc si
+;         jmp ciclo
+
+;     ciclo2: 
+;         ;aqui identifico que es una vocal
+        
+;         imprimir salto
+;         imprimir letra
+;         imprimir salto
+
+;         inc di
+;         xor si, si
+
+;         cmp palabra[di], "$"
+;         jz fini
+
+;         jmp ciclo
+
+;     ciclo3:
+;         xor si, si
+       
+;     ciclo4:
+;         mov bl, vcerrada[si]
+;         cmp palabra[di], bl
+;         jz ciclo2
+
+;         cmp si, 1
+;         jz ciclo5
+
+;         inc si
+;         jmp ciclo4
+
+;     ciclo5:
+;         inc di
+;         xor si, si
+;         cmp palabra[di], "$"
+;         jz fini
+
+;         jmp ciclo
+
+
+  
+
+;     fini:
+;         imprimir salto
+;         imprimir fin
+;         imprimir salto
+        
+
+    
+
+
+; endm
+
+
+
+veridip macro palabra
+    local comparar, diferente, iguales, final
+    xor di, di
+    xor bh, bh
+    xor bl, bl
+    comparar:
+        mov bh, palabra[di]
+        mov bl, condip[di]
+        cmp bh, bl
+        ; sino son iguales
+        jnz diferente
+
+        cmp bl, "$" ;si es igual a dolar
+        jz iguales
+        ; si son iguales
+        inc di
+        jmp comparar
+
+    iguales:
+        ;en esta parte hace las acciones respectivas del comando 
+        mov contarW, 1d        
+        jmp final
+    diferente: 
+        
+        ; lo ignora y pasa a la siguiente verificacion
+        jmp final
+    final:  
+endm
+
+veritrip macro palabra
+    local comparar, diferente, iguales, final
+    xor di, di
+    xor bh, bh
+    xor bl, bl
+    comparar:
+        mov bh, palabra[di]
+        mov bl, contrip[di]
+        cmp bh, bl
+        ; sino son iguales
+        jnz diferente
+
+        cmp bl, "$" ;si es igual a dolar
+        jz iguales
+        ; si son iguales
+        inc di
+        jmp comparar
+
+    iguales:
+        ;en esta parte hace las acciones respectivas del comando 
+        mov contarW, 2d     
+        jmp final
+    diferente: 
+        
+        ; lo ignora y pasa a la siguiente verificacion
+        jmp final
+    final:  
+endm
+
+verihia macro palabra
+    local comparar, diferente, iguales, final
+    xor di, di
+    xor bh, bh
+    xor bl, bl
+    comparar:
+        mov bh, palabra[di]
+        mov bl, conhia[di]
+        cmp bh, bl
+        ; sino son iguales
+        jnz diferente
+
+        cmp bl, "$" ;si es igual a dolar
+        jz iguales
+        ; si son iguales
+        inc di
+        jmp comparar
+
+    iguales:
+        ;en esta parte hace las acciones respectivas del comando 
+        mov contarW, 3d
+        jmp final
+    diferente: 
+         
+        ; lo ignora y pasa a la siguiente verificacion
+        jmp final
+    final:  
+endm
+
+veripal macro palabra
+    local comparar, diferente, iguales, final
+    xor di, di
+    xor bh, bh
+    xor bl, bl
+    comparar:
+        mov bh, palabra[di]
+        mov bl, conpal[di]
+        cmp bh, bl
+        ; sino son iguales
+        jnz diferente
+
+        cmp bl, "$" ;si es igual a dolar
+        jz iguales
+        ; si son iguales
+        inc di
+        jmp comparar
+
+    iguales:
+        ;en esta parte hace las acciones respectivas del comando 
+        mov contarW, 4d        
+        jmp final
+    diferente: 
+      
+        ; lo ignora y pasa a la siguiente verificacion
+        jmp final
+    final:  
+          
+endm
+
+numPalabra macro ;textFile
+    local ciclo, ciclo2, ciclo3, ciclo4, finito
+    local cicloLimpiar, finLimpiar, chauDip, chauDipd
+
+    xor si, si
+    xor di, di
+
+    ciclo:
+        mov al, textFile[si]
+
+        
+    
+        cmp al, spc[0]
+        jz espacio
+
+        mov anaWord[di], al
+        ;guardando en otra variable la letra que esta leyendo
+
+        cmp al, "$"
+        jz finito
+
+        
+        inc si
+        inc di
+        jmp ciclo
+
+
+
+    espacio:
+        ; imprimir salto
+        ; imprimir spcDos
+        ; imprimir salto
+        ; imprimir anaWord
+        ; imprimir salto
+        mov indiceK, si ;guardo el valor de si porque al entrar a identifyDipto se reinicia
+        inc di
+        mov anaWord[di], "$"
+
+
+        ;verificar si la palabra es triptongo
+        identifyTripto anaWord
+        cmp flagYa, 1d
+        jz chauDip
+
+        ;verificar si la palabra es diptongo 
+        identifyDipto anaWord
+        
+        chauDip:
+            ;verificar si la palabra hiato
+            identifyHiato anaWord
+
+
+       
+
+        ; ---------------------------------------------
+        ; ESTE BLOQUE LIMPIA LA ENTRADA PARA SEGUIR GUARDANDO
+        ; PALABRAS TEMPORALES
+        xor di, di
+        cicloLimpiar:
+            mov bl, "$"
+            mov anaWord[di], bl
+            
+            cmp di, 49d
+            je finLimpiar
+            inc di
+            jmp cicloLimpiar
+        finLimpiar:
+        ; ---------------------------------------------
+
+        mov flagYa, 0d
+        mov si, indiceK
+        inc numPal ; incrementando el numero de palabras
+        xor di, di
+        inc si
+        jmp ciclo
+
+    finito:
+        ;aqui tambien tengo que hacer validaciones porque la ultima 
+        ;palabra aun queda guardada y aca la leo
+
+        inc di
+        mov anaWord[di], "$"
+
+        ;verificar si la palabra es triptongo
+        identifyTripto anaWord
+        cmp flagYa, 1d
+        jz chauDipd
+
+        ;verificar si la palabra es diptongo 
+        identifyDipto anaWord
+        
+        chauDipd:
+            ;verificar si la palabra hiato
+            identifyHiato anaWord
+       
+        
+        ; imprimir salto
+        ; imprimir spcDos
+        ; imprimir salto
+        ; imprimir anaWord
+        ; imprimir salto
+    
+endm
--- Practica4_201807307/Codigo_asm/practica.asm
+++ Practica4_201807307/Codigo_asm/practica.asm
@@ -35,6 +35,62 @@ sdatos segment
 	ncomando db 0d
 	yes db "entro a ", "$"
 
+	; lectura de archivo 
+	msgtxt db "Contenido del archivo: ", "$"
+	textFile db 2222 dup("$"),"$"
+	handle dw 0
+	ruta db 50 dup("$"), 0
+	nextWord db 50 dup("$"), "$"
+	indiceI dw 0 ;indice utilizado en la ruta
+	indiceJ dw 0 ;indice utilizado en nextWord
+	indiceK dw 0
+
+	; clasificar en diptongo, triptongo e hiato
+	vocales db "aeiou","$"
+	vabierta db "aeo","$"
+	vcerrada db "iu","$"
+
+	groupV db 10 dup("$"), "$"
+	flagD db 0
+	flagT db 0
+	flagH db 0
+	flagW db 0
+	ite dw 0
+	cambioL db "cambio letra","$"
+	letra db "igualo letra","$"
+	oldVoc db " ","$"
+	newVoc db " ","$"
+	fin db "fin","$"
+
+	;tipo de cada palabra
+	wordDip db "Diptongo ","$"
+	crecien db "- Creciente","$"
+	decrec db "- Decreciente","$"
+	homoge db "- Homogeneo","$"
+	noWord db "No es ","$"
+	hiatoS db "Hiato - Simple", "$"
+	noHiato db "No es Hiato","$"
+	tripton db "Triptongo", "$"
+
+	;comando contar
+	contarW db 0d
+	condip db "diptongo","$"
+	contrip db "triptongo","$"
+	conhia db "hiato", "$"
+	conpal db "palabra", "$"
+	spc db " ","$"
+	spcDos db "espacio en blanco","$"
+	numDip dw 0d
+	numTrip dw 0d
+	numHia dw 0d
+	numPal dw 1d
+	anaWord db 50 dup("$"), "$"
+	flagCount db 0
+	flagYa db 0
+	
+
+
+
 
 	
 
@@ -44,7 +100,45 @@ sdatos ends
 
 scodigo segment 'CODE'
     
-	ASSUME SS:spila, DS:sdatos, CS:scodigo         
+	ASSUME SS:spila, DS:sdatos, CS:scodigo  
+	IMPRIMIR_NUMERO proc
+		; AX -> parametro donde voy a poner el número a imprimir
+		push dx
+		push si
+		push di
+		push cx
+		push bx
+
+		mov cx, '$'
+		push cx
+		for1num:
+			xor dx, dx
+			mov bx, 10d
+			div bx
+			push dx
+			cmp ax, 10
+			jge for1num		
+			cmp ax, 0
+			je axEs0
+		push ax
+
+		axEs0:
+		for2num:
+			pop dx
+			cmp dl, '$'
+			je finFor2
+			mov ah, 02h
+			add dl, 30h
+			int 21h
+			jmp for2num
+		finFor2:
+		pop bx
+		pop cx
+		pop di
+		pop si
+		pop dx
+		ret
+	IMPRIMIR_NUMERO endp        
 	
 	
 	main proc far 
@@ -108,14 +202,6 @@ scodigo segment 'CODE'
 
 
 
-
-
-
-
-
-
-
-
 			;verificar si el comando es x
 			mov al, readTeclado[0]
 			cmp al, wordx
@@ -134,15 +220,106 @@ scodigo segment 'CODE'
 				imprimir salto
 				imprimir wordabrir
 				imprimir salto
+
+				getRuta ;macro para obtener el nombre de la ruta 
+				imprimir ruta
+				imprimir salto
+
+				imprimir msgtxt
+				imprimir salto	
+				abrirArchivo ruta
+				;leyendo contenido de archivo
+				mov ah, 3Fh
+				mov bx, handle
+				mov cx, 2222
+				mov dx, offset textFile
+				int 21h
+				cerrarArchivo handle
+				;mostrando texto del archivo
+				imprimir textFile
+				
+
+
 				jmp cleanAll
 
 			comando_contar: 
+				mov flagCount, 1d
+
 				imprimir salto
 				imprimir yes
 				imprimir salto
 				imprimir wordcontar
 				imprimir salto
+
+				numPalabra
+
+				getWord
+				imprimir nextWord
+				imprimir salto
+
+				veridip nextWord
+				veritrip nextWord
+				verihia nextWord
+				veripal nextWord
+
+				mov al, contarW
+				cmp al, 1d ;diptongo
+				jz countDip
+
+				cmp al, 2d ;triptongo
+				jz countTrip
+
+				cmp al, 3d ;hiato
+				jz countHia
+
+				cmp al, 4d ;palabra
+				jz countPal
+
+				jmp comando_error
+
+				
+				countDip:
+					imprimir salto
+					imprimir condip
+					imprimir salto
+					mov ax, numDip
+					call IMPRIMIR_NUMERO 
+					jmp cleanAll
+				
+
+				countTrip:
+					imprimir salto
+					imprimir contrip
+					imprimir salto
+					mov ax, numTrip
+					call IMPRIMIR_NUMERO 
+					jmp cleanAll
+		
+
+
+				countHia:
+					imprimir salto
+					imprimir conhia
+					imprimir salto
+					mov ax, numHia
+					call IMPRIMIR_NUMERO 
+					jmp cleanAll
+
+				countPal:
+					imprimir salto
+					imprimir conpal
+					imprimir salto
+					mov ax, numPal
+					call IMPRIMIR_NUMERO 
+					jmp cleanAll
+
 				jmp cleanAll
+
+				
+
+
+
+				
 
 			comando_prop: 
 				imprimir salto
@@ -169,27 +346,54 @@ scodigo segment 'CODE'
 				jmp cleanAll
 
 			comando_diptongo: 
+				mov flagCount, 0d
 				imprimir salto
 				imprimir yes
 				imprimir salto
 				imprimir worddipto
 				imprimir salto
+
+				getWord
+				imprimir nextWord
+				imprimir salto
+
+				identifyDipto nextWord
+
+
+
+
 				jmp cleanAll
 
 			comando_hiato: 
+				mov flagCount, 0d
 				imprimir salto
 				imprimir yes
 				imprimir salto
 				imprimir wordhiato
 				imprimir salto
+
+				getWord
+				imprimir nextWord
+				imprimir salto
+
+				identifyHiato nextWord
+
 				jmp cleanAll
 
 			comando_triptongo: 
+				mov flagCount, 0d
 				imprimir salto
 				imprimir yes
 				imprimir salto
 				imprimir wordtripto
 				imprimir salto
+
+				getWord
+				imprimir nextWord
+				imprimir salto
+
+				identifyTripto nextWord
+
 				jmp cleanAll
 
 			comando_error:
@@ -198,11 +402,26 @@ scodigo segment 'CODE'
 				imprimir salto
 			
 				
-			cleanAll:
+			cleanAll: ;este salto me permite hacer los clear necesarios para que todo funcione bien 
+					  ;al seguir ejecutando 
+
 				; limpiando pantalla y entrada
 				leerHastaEnter pauseEnter 
 				limpiarEntrada readTeclado ;para que no haya problemas con el nuevo comando
+				limpiarEntrada ruta
+				limpiarEntrada nextWord 
+				;limpiarEntrada nextWord
 				mov ncomando, 0d ;la opcion del comando seleccionado
+				mov indiceI, 0d
+				mov indiceJ,0d
+				mov contarW, 0d
+				;limpiando contador de palabras
+				;para que no se duplique el punteo 
+				mov numDip, 0d
+				mov numHia, 0d
+				mov numTrip, 0d
+				mov numPal, 1d
+				xor ax, ax
 				limpiarT ;limpiar pantalla
 				
 				jmp repeticion
